3906e612890bc1eed6fcea4bb374e8fb04293e6a
pjsier <pjsier@gmail.com>
2017-05-06 14:11:51 -0500

    cleanup js

--- reporter/static/js/script.js
+++ reporter/static/js/script.js
@@ -1,103 +1,101 @@
 var svg, width, height, projection, path, spinner, tooltip, cbColors;
 
-function handleGeoJson(geoPath) {
-  d3.json(geoPath, function(json) {
-    if (!svg.select("g.chicago").selectAll("path").size()) {
-      var bounds = path.bounds(json);
-      var s = 0.95 / Math.max((bounds[1][0] - bounds[0][0]) / width, (bounds[1][1] - bounds[0][1]) / height);
-      var t = [(width - s * (bounds[1][0] + bounds[0][0])) / 2, (height - s * (bounds[1][1] + bounds[0][1])) / 2];
-      projection.scale(s).translate(t);
-      tooltip = d3.select("body")
-        .append("div")
-        .style("visibility", "hidden")
-        .style("font-size", "16px")
-        .style("padding", "10px")
-        .style("z-index", "10")
-        .style("position", "absolute")
-        .style("background-color", "lightgrey");
-    }
-    else {
-      svg.select("g.chicago")
-        .selectAll("path").remove();
-      svg.select("g.legend").remove();
-    }
-
-    var color = d3.scaleQuantize()
-        .domain([0, d3.max(json.features, function(d) {
-          if (d.properties.ci_count) {
-            return d.properties.ci_count;
-          }
-          return 0;
-        })])
-        .range(colorbrewer[cbColors][5]);
-
-    var legend = d3.select('svg')
-        .append("g")
-          .attr("class", "legend")
-        .selectAll("g")
-        .data(color.range())
-        .enter()
-        .append('g')
-          .attr('class', 'legend-item')
-          .attr('transform', function(d, i) {
-            var h = 25;
-            var x = 0;
-            var y = (i * h) + (height * 0.7);
-            return 'translate(' + x + ',' + y + ')';
-        });
-
-    legend.append('rect')
-        .attr('width', 25)
-        .attr('height', 25)
-        .style('fill', function(d) { return d; });
-
-    var legendText = "Calls/Issues by ";
-    if (json.features[0].properties.zip) {
-      var tooltA = "Zip";
-      legendText += tooltA;
-      var areaProp = "zip";
-    }
-    else if (json.features[0].properties.ward) {
-      var tooltA = "Ward";
-      legendText += tooltA;
-      var areaProp = "ward";
-    }
-
-    svg.select('g.legend').append("text")
-      .attr('x', 0)
-      .attr('y', -15)
-      .attr('font-weight', 'bold')
-      .text(legendText)
-      .attr('transform', 'translate(0,' + (height*0.7) + ')');
-
-    legend.append('text')
-        .attr('x', 35)
-        .attr('y', 20)
-        .text(function(d) {
-          return color.invertExtent(d).map(function(d) { return Math.floor(d); }).join("-");
-        });
-
+function handleGeoJson(json) {
+  if (!svg.select("g.chicago").selectAll("path").size()) {
+    var bounds = path.bounds(json);
+    var s = 0.95 / Math.max((bounds[1][0] - bounds[0][0]) / width, (bounds[1][1] - bounds[0][1]) / height);
+    var t = [(width - s * (bounds[1][0] + bounds[0][0])) / 2, (height - s * (bounds[1][1] + bounds[0][1])) / 2];
+    projection.scale(s).translate(t);
+    tooltip = d3.select("body")
+      .append("div")
+      .style("visibility", "hidden")
+      .style("font-size", "16px")
+      .style("padding", "10px")
+      .style("z-index", "10")
+      .style("position", "absolute")
+      .style("background-color", "lightgrey");
+  }
+  else {
     svg.select("g.chicago")
-      .selectAll("path")
-        .data(json.features, function(d) {
-          return d.properties[areaProp] + "-" + d.properties.ci_count;
-        })
-        .enter().append("path")
-          .attr("d", path)
-          .attr("fill-opacity", 0.8)
-          .attr("stroke", "#6F7070")
-          .attr("stroke-opacity", 0.8)
-          .attr("stroke-width", 1)
-          .attr("fill", function(d) { return color(d.properties.ci_count);})
-          .on("mouseover", function(d){
-            return tooltip.style("visibility", "visible")
-              .html("<b>" + tooltA + "</b>: " + d.properties[areaProp] + "<br><b>Count</b>: " + d.properties.ci_count)
-          })
-	        .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-	        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
+      .selectAll("path").remove();
+    svg.select("g.legend").remove();
+  }
 
-    spinner.style.display = "none";
-  });
+  var color = d3.scaleQuantize()
+      .domain([0, d3.max(json.features, function(d) {
+        if (d.properties.ci_count) {
+          return d.properties.ci_count;
+        }
+        return 0;
+      })])
+      .range(colorbrewer[cbColors][5]);
+
+  var legend = d3.select('svg')
+      .append("g")
+        .attr("class", "legend")
+      .selectAll("g")
+      .data(color.range())
+      .enter()
+      .append('g')
+        .attr('class', 'legend-item')
+        .attr('transform', function(d, i) {
+          var h = 25;
+          var x = 0;
+          var y = (i * h) + (height * 0.7);
+          return 'translate(' + x + ',' + y + ')';
+      });
+
+  legend.append('rect')
+      .attr('width', 25)
+      .attr('height', 25)
+      .style('fill', function(d) { return d; });
+
+  var legendText = "Calls/Issues by ";
+  if (json.features[0].properties.zip) {
+    var tooltA = "Zip";
+    legendText += tooltA;
+    var areaProp = "zip";
+  }
+  else if (json.features[0].properties.ward) {
+    var tooltA = "Ward";
+    legendText += tooltA;
+    var areaProp = "ward";
+  }
+
+  svg.select('g.legend').append("text")
+    .attr('x', 0)
+    .attr('y', -15)
+    .attr('font-weight', 'bold')
+    .text(legendText)
+    .attr('transform', 'translate(0,' + (height*0.7) + ')');
+
+  legend.append('text')
+      .attr('x', 35)
+      .attr('y', 20)
+      .text(function(d) {
+        return color.invertExtent(d).map(function(d) { return Math.floor(d); }).join("-");
+      });
+
+  svg.select("g.chicago")
+    .selectAll("path")
+      .data(json.features, function(d) {
+        return d.properties[areaProp] + "-" + d.properties.ci_count;
+      })
+      .enter().append("path")
+        .attr("d", path)
+        .attr("fill-opacity", 0.8)
+        .attr("stroke", "#6F7070")
+        .attr("stroke-opacity", 0.8)
+        .attr("stroke-width", 1)
+        .attr("fill", function(d) { return color(d.properties.ci_count);})
+        .on("mouseover", function(d){
+          return tooltip.style("visibility", "visible")
+            .html("<b>" + tooltA + "</b>: " + d.properties[areaProp] + "<br><b>Count</b>: " + d.properties.ci_count)
+        })
+        .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
+
+  spinner.style.display = "none";
 }
 
 //pads left
@@ -171,6 +169,6 @@ String.prototype.lpad = function(padString, length) {
     var printLink = document.getElementById("printLink");
     printLink.href = "print?" + queryArgs.join("&");
 
-    handleGeoJson(queryUrl);
+    d3.json(queryUrl, handleGeoJson);
   });
 })()
